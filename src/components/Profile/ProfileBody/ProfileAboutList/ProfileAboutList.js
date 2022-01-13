3a9b4f51d5c533c55e93fbacb471a8ab09ace402
5z3wczykj4kub <5z3wczykj4kub@gmail.com>
2022-01-13 16:58:31 +0100

    update about info

--- src/components/Profile/ProfileBody/ProfileAboutList/ProfileAboutList.js
+++ src/components/Profile/ProfileBody/ProfileAboutList/ProfileAboutList.js
@@ -1,24 +1,53 @@
-import styles from './ProfileAboutList.module.scss';
-import getAgeByDateOfBirth from '../../../../utils/getAgeByDateOfBirth';
-import { Link } from 'react-router-dom';
+import { useRef, useState } from 'react';
 import copyIcon from '../../../../assets/copy.png';
+import profileIcon from '../../../../assets/profile.png';
+import getAgeByDateOfBirth from '../../../../utils/getAgeByDateOfBirth';
+import styles from './ProfileAboutList.module.scss';
 
-const ProfileAboutList = ({ id, firstName, lastName, dateOfBirth }) => {
-  getAgeByDateOfBirth(dateOfBirth);
+const ProfileAboutList = ({
+  id,
+  firstName,
+  lastName,
+  email,
+  gender,
+  dateOfBirth,
+  location,
+}) => {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+  const timer = useRef();
+
+  const profileLink = `http://localhost:3000/profiles/${id}`;
+
+  const copyProfileLinkToClipboard = async () => {
+    clearTimeout(timer.current);
+    await navigator.clipboard.writeText(profileLink);
+    setIsTooltipVisible(true);
+    timer.current = setTimeout(() => setIsTooltipVisible(false), 1000);
+  };
 
   return (
     <div className={styles.ProfileAboutList}>
+      <h2>
+        <img src={profileIcon} alt="Profile" /> Personal information
+      </h2>
       <p>First name: {firstName}</p>
       <p>Last name: {lastName}</p>
+      <p>Email: {email}</p>
+      <p>Gender: {gender}</p>
       <p>Age: {getAgeByDateOfBirth(dateOfBirth)}</p>
       <p>Date of birth: {dateOfBirth}</p>
+      <p>Location: {location}</p>
       <p className={styles.profileLink}>
-        Profile link:
-        <Link
-          className={styles.Link}
-          to={`/profiles/${id}`}
-        >{`http://localhost:3000/profiles/${id}`}</Link>
-        <img className={styles.copyIcon} src={copyIcon} alt="copy" />
+        Copy profile link:
+        <span>
+          <img
+            className={styles.copyIcon}
+            src={copyIcon}
+            alt="copy"
+            onClick={copyProfileLinkToClipboard}
+          />
+          {isTooltipVisible && <span className={styles.tooltip}>Copied!</span>}
+        </span>
       </p>
     </div>
   );
